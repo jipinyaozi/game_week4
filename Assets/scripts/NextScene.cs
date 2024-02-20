f59c6e419ffc78146af29b8abe068f51992a44b8
catcatalmuete <ca2318@nyu.edu>
2024-02-20 14:29:20 -0500

    NextScene Object (Buggy)

---
+++ Assets/scripts/NextScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    // Name of the scene to transition to
+    [SerializeField] private string nextSceneName;
+
+    // Function to transition to the next scene
+    public void TransitionToNextScene()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    // Trigger the transition when the player enters the trigger collider
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TransitionToNextScene();
+        }
+    }
+}
