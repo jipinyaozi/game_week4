896a3d7328e67ea84695870f15dc14ec873519c9
Cat Almuete <ca2318@nyu.edu>
2024-04-24 13:48:13 -0400

    Finished Level 3 + New Tag

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -208,7 +208,7 @@ public class Movement : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         changetag();
-        clone.tag = "Player";
+        clone.tag = "Main Player";
 
     }
 
